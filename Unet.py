da0f2647076d30edb0072c64ffeba5039a162bf7
TheRoenizer <christoffer.roen@gmail.com>
2020-02-12 10:16:59 +0100

    show pred 1.0

--- Unet.py
+++ Unet.py
@@ -15,9 +15,9 @@ except:
 
 
 # Christoffer:
-# PATH = 'C:/Users/chris/'
+PATH = 'C:/Users/chris/'
 # Jonathan:
-PATH = '/Users/jonathansteen/'
+#PATH = '/Users/jonathansteen/'
 
 
 def unet(input_shape, num_classes=1, droprate=None, linear=False):
@@ -125,7 +125,7 @@ for i in range(80, 90):
     imgs_val[i-80] = img
     print(imgs_val)
 
-# Labels for right gripper
+# Labels for left arm or something else
 lbls_train = np.zeros((79, 480, 640))
 for i in range(1, 80):
     print('Progress: ' + str(i) + ' of 79')
@@ -168,8 +168,12 @@ def create_mask(pred_mask):
 
 
 def show_predictions(image_num):
-    pred_mask = unet.predict(imgs_train[image_num])
-    display([imgs_train[image_num], lbls_train[image_num], create_mask(pred_mask)])
+    pred_mask = unet.predict(imgs_train[image_num][tf.newaxis, ...]) * 255
+    #print(pred_mask.shape)
+    display([imgs_train[image_num], lbls_train[image_num], pred_mask[0]])
 
+show_predictions(1)
 
 unet.fit(imgs_train, lbls_train, validation_data=[imgs_val, lbls_val], batch_size=1, epochs=1, verbose=1, shuffle=True)
+
+show_predictions(1)
