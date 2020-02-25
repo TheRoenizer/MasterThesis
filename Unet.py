6618ecaa863f228a842457f1d2d4fd618b5dd8e4
TheRoenizer <christoffer.roen@gmail.com>
2020-02-25 08:56:21 +0100

    save image

--- Unet.py
+++ Unet.py
@@ -97,13 +97,14 @@ def unet(input_shape, num_classes=5, droprate=None, linear=False):
 def display(display_list):
     plt.figure(figsize=(15, 15))
 
-    title = ['Input Image', 'True Mask shaft', 'True Mask gripper', 'Predicted Mask']
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
 
     for i in range(len(display_list)):
         plt.subplot(1, len(display_list), i+1)
         plt.title(title[i])
         plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
         plt.axis('off')
+    plt.savefig("afterEpoch{}.png".format(epoch))
     plt.show()
 
 
