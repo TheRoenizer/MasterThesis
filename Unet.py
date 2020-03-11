66ebcacb9db5eff571f4760cd4e9de5c4f0c617c
Jonathan Steen <joste15@student.sdu.dk>
2020-03-11 12:14:33 +0100

    sdur-3

--- Unet.py
+++ Unet.py
@@ -1,10 +1,9 @@
-"""
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
-"""
+
 import tensorflow as tf
 from keras import backend as K
 import numpy as np
