4a663f9cf14367165abb3e83585395427eb12a29
Jonathan Steen <joste15@student.sdu.dk>
2020-04-16 15:20:28 +0200

    pose test

--- extract_rosbag.py
+++ extract_rosbag.py
@@ -153,4 +153,4 @@ with rosbag.Bag(path) as bag:
         t_base_tcp = msg2tf(psm1_msg.pose)
         t_optical_tcp = t_optical_base.dot(t_base_tcp)
 
-        print(t_optical_tcp[0[0]])
+        print(t_optical_tcp[1[1]])
