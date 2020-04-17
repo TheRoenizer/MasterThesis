d54407cab83ee7b746515c4a041ebad62d1eecce
Jonathan Steen <joste15@student.sdu.dk>
2020-04-17 15:30:33 +0200

    NewTest

--- extract_rosbag.py
+++ extract_rosbag.py
@@ -129,7 +129,7 @@ with rosbag.Bag(path) as bag:
     # Read all PSM1 pose messages (instrument TCP wrt. base frame) PSM = patient side manipulator
     psm1_msgs = [msg for topic, msg, stamp in bag.read_messages(topics=['/dvrk/PSM1/position_cartesian_current'])]
 
-    poses = np.zeros((4, 4, 1))
+    poses = np.zeros((4, 4))
 
     for i in range(60, 1400, 20):
         # Get the i'th right camera image message in the bag
@@ -159,7 +159,7 @@ with rosbag.Bag(path) as bag:
         t_optical_tcp = t_optical_base.dot(t_base_tcp)
 
         print(t_optical_tcp)
-        poses = np.concatenate([poses, t_optical_tcp], axis=-1)
+        poses = np.concatenate([np.atleast_3d(poses), np.atleast_3d(t_optical_tcp)], axis=-1)
         # poses = np.append(np.atleast_3d(poses), np.atleast_3d(t_optical_tcp), axis=-1)
         print(poses.shape)
 
