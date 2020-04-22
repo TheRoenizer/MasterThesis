e64e494b8bf5970cc5a3c168f499eabdf1e9bc2f
Jonathan Steen <joste15@student.sdu.dk>
2020-04-22 11:24:26 +0200

    typo fix

--- extract_rosbag.py
+++ extract_rosbag.py
@@ -182,7 +182,7 @@ with rosbag.Bag(path) as bag:
     # Read all PSM1 pose messages (instrument TCP wrt. base frame) PSM = patient side manipulator
     psm1_msgs = [msg for topic, msg, stamp in bag.read_messages(topics=['/dvrk/PSM1/position_cartesian_current'])]
 
-    poses = np.zeros((4, 4))
+    # poses = np.zeros((4, 4))
 
     for i in range(35, 1370, 20):
         # Get the i'th right camera image message in the bag
@@ -238,7 +238,7 @@ with rosbag.Bag(path) as bag:
     # Read all PSM1 pose messages (instrument TCP wrt. base frame) PSM = patient side manipulator
     psm1_msgs = [msg for topic, msg, stamp in bag.read_messages(topics=['/dvrk/PSM1/position_cartesian_current'])]
 
-    poses = np.zeros((4, 4))
+    # poses = np.zeros((4, 4))
 
     for i in range(120, 264, 2):
         # Get the i'th right camera image message in the bag
@@ -294,7 +294,7 @@ with rosbag.Bag(path) as bag:
     # Read all PSM1 pose messages (instrument TCP wrt. base frame) PSM = patient side manipulator
     psm1_msgs = [msg for topic, msg, stamp in bag.read_messages(topics=['/dvrk/PSM1/position_cartesian_current'])]
 
-    poses = np.zeros((4, 4))
+    # poses = np.zeros((4, 4))
 
     for i in range(110, 209, 2):
         # Get the i'th right camera image message in the bag
