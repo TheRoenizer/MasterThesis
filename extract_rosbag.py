5188ac4f28add499493c804946fef66889f8eae4
Jonathan Steen <joste15@student.sdu.dk>
2020-04-22 11:52:17 +0200

    rosbag order fix

--- extract_rosbag.py
+++ extract_rosbag.py
@@ -126,7 +126,7 @@ with rosbag.Bag(path) as bag:
     # Read all PSM1 pose messages (instrument TCP wrt. base frame) PSM = patient side manipulator
     psm1_msgs = [msg for topic, msg, stamp in bag.read_messages(topics=['/dvrk/PSM1/position_cartesian_current'])]
 
-    # poses = np.zeros((4, 4))
+    poses = np.zeros((4, 4))
 
     for i in range(120, 264, 2):
         # Get the i'th right camera image message in the bag
@@ -238,7 +238,7 @@ with rosbag.Bag(path) as bag:
     # Read all PSM1 pose messages (instrument TCP wrt. base frame) PSM = patient side manipulator
     psm1_msgs = [msg for topic, msg, stamp in bag.read_messages(topics=['/dvrk/PSM1/position_cartesian_current'])]
 
-    poses = np.zeros((4, 4))
+    # poses = np.zeros((4, 4))
 
     for i in range(60, 1400, 20):
         # Get the i'th right camera image message in the bag
