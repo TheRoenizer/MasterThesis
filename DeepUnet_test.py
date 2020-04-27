8a944683c2c9f4d1e0fc07eb220f714d4ab68a1a
TheRoenizer <christoffer.roen@gmail.com>
2020-04-27 12:55:14 +0200

    Splitted deepunet to more files

---
+++ DeepUnet_test.py
@@ -0,0 +1,205 @@
+import os
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+
+# Hvis du vil bruge "kort 1":
+#os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+# ellers:
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+# hvis du træne på CPU'en:
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+from DeepUnet import *
+from Loss_functions import *
+
+which_path = 1 # 1 = local, 2 = remote
+batch_size = 1
+num_epochs = 100
+#num_pixels = 480 * 640
+weights = [.5, 1.5, 1.5, 1, 1] # [background, gripper, gripper, shaft, shaft]
+
+if which_path == 1:
+    # Christoffer:
+    PATH = 'C:/Users/chris/Google Drive/'
+elif which_path == 2:
+    # Linux:
+    PATH = '/home/jsteeen/'
+
+# functions used to display images after each epoch
+def display(display_list, epoch_display):
+    fig = plt.figure(figsize=(15, 15))
+
+    title = ['Input Image', 'True Mask', 'Predicted Mask after epoch {}'.format(epoch_display + 1)]
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i + 1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.savefig("Pictures_DeepUnet/afterEpoch{}.png".format(epoch_display + 1))
+    # plt.show()
+    plt.close(fig)
+
+def create_mask(pred_mask):
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
+
+def show_predictions(epoch_show_predictions, image_num=1):
+    pred_mask = deep_unet.predict(imgs_val[image_num][tf.newaxis, ...]) * 255
+    display([imgs_val[image_num], lbls_val[image_num], create_mask(pred_mask)], epoch_show_predictions)
+
+class DisplayCallback(tf.keras.callbacks.Callback):
+    # @staticmethod
+    def on_epoch_end(self, epoch_callback, logs=None):
+        clear_output(wait=True)
+        show_predictions(epoch_callback)
+        print('\nSample Prediction after epoch {}\n'.format(epoch_callback + 1))
+
+
+# Load images
+imgs_train = np.zeros((79, 480, 640, 3))
+print('Loading images...')
+for i in range(1, 80):
+    # print('Progress: ' + str(i) + ' of 79')
+    path = PATH + '/Jigsaw annotations/Images/Suturing (' + str(i) + ').png'
+    img = np.array(Image.open(path))[np.newaxis]
+    img = img / 255
+    imgs_train[i-1] = img
+
+imgs_val = np.zeros((10, 480, 640, 3))
+for i in range(80, 90):
+    # print('Progress: ' + str(i) + ' of 89')
+    path = PATH + '/Jigsaw annotations/Images/Suturing (' + str(i) + ').png'
+    img = np.array(Image.open(path))[np.newaxis]
+    img = img / 255
+    imgs_val[i-80] = img
+
+imgs_test = np.zeros((10, 480, 640, 3))
+for i in range(90, 100):
+    # print('Progress: ' + str(i) + ' of 89')
+    path = PATH + '/Jigsaw annotations/Images/Suturing (' + str(i) + ').png'
+    img = np.array(Image.open(path))[np.newaxis]
+    img = img / 255
+    imgs_test[i-90] = img
+
+print('Images loaded!')
+print('Loading labels...')
+
+# Load labels
+lbls_train = np.zeros((79, 480, 640))
+sample_weight = np.zeros((79, 480, 640))
+for i in range(1, 80):
+    # print('Progress: ' + str(i) + ' of 79')
+    path1 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/000.png'
+    path2 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/002.png'
+    path3 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/003.png'
+    path4 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/001.png'
+    img1 = cv.imread(path1, 2)
+    img2 = cv.imread(path2, 2)
+    img3 = cv.imread(path3, 2)
+    img4 = cv.imread(path4, 2)
+    change1_to = np.where(img1[:, :] != 0)
+    change2_to = np.where(img2[:, :] != 0)
+    change3_to = np.where(img3[:, :] != 0)
+    change4_to = np.where(img4[:, :] != 0)
+    img1[change1_to] = 1
+    img2[change2_to] = 2
+    img3[change3_to] = 3
+    img4[change4_to] = 4
+    weight1 = np.zeros((480, 640))
+    weight2 = np.zeros((480, 640))
+    weight3 = np.zeros((480, 640))
+    weight4 = np.zeros((480, 640))
+    weight1[change1_to] = 10
+    weight2[change2_to] = 1
+    weight3[change3_to] = 10
+    weight4[change4_to] = 1
+    img = img1 + img2 + img3 + img4
+    weight = weight1 + weight2 + weight3 + weight4
+    change_5 = np.where(img[:, :] == 5)
+    img[change_5] = 0
+    change_overlap = np.where(weight[:, :] == 11)
+    weight[change_overlap] = 0
+    lbls_train[i-1] = img
+    sample_weight[i-1] = weight
+
+lbls_train_onehot = tf.keras.utils.to_categorical(lbls_train, num_classes=5, dtype='float32')
+lbls_train = lbls_train.reshape((79, 480, 640, -1))
+
+lbls_val = np.zeros((10, 480, 640))
+for i in range(80, 90):
+    # print('Progress: ' + str(i) + ' of 89')
+    path1 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/000.png'
+    path2 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/002.png'
+    path3 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/003.png'
+    path4 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/001.png'
+    img1 = cv.imread(path1, 2)
+    img2 = cv.imread(path2, 2)
+    img3 = cv.imread(path3, 2)
+    img4 = cv.imread(path4, 2)
+    change1_to = np.where(img1[:, :] != 0)
+    change2_to = np.where(img2[:, :] != 0)
+    change3_to = np.where(img3[:, :] != 0)
+    change4_to = np.where(img4[:, :] != 0)
+    img1[change1_to] = 1
+    img2[change2_to] = 2
+    img3[change3_to] = 3
+    img4[change4_to] = 4
+    img = img1 + img2 + img3 + img4
+    change_5 = np.where(img[:, :] == 5)
+    img[change_5] = 0
+    lbls_val[i-80] = img
+
+lbls_val_onehot = tf.keras.utils.to_categorical(lbls_val, num_classes=5, dtype='float32')
+lbls_val = lbls_val.reshape((10, 480, 640, -1))
+
+lbls_test = np.zeros((10, 480, 640))
+for i in range(90, 100):
+    # print('Progress: ' + str(i) + ' of 89')
+    path1 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/000.png'
+    path2 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/002.png'
+    path3 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/003.png'
+    path4 = PATH + '/Jigsaw annotations/Annotated/Suturing (' + str(i) + ')' + '/data/001.png'
+    img1 = cv.imread(path1, 2)
+    img2 = cv.imread(path2, 2)
+    img3 = cv.imread(path3, 2)
+    img4 = cv.imread(path4, 2)
+    change1_to = np.where(img1[:, :] != 0)
+    change2_to = np.where(img2[:, :] != 0)
+    change3_to = np.where(img3[:, :] != 0)
+    change4_to = np.where(img4[:, :] != 0)
+    img1[change1_to] = 1
+    img2[change2_to] = 2
+    img3[change3_to] = 3
+    img4[change4_to] = 4
+    img = img1 + img2 + img3 + img4
+    change_5 = np.where(img[:, :] == 5)
+    img[change_5] = 0
+    lbls_test[i - 90] = img
+
+lbls_test_onehot = tf.keras.utils.to_categorical(lbls_test, num_classes=5, dtype='float32')
+lbls_test = lbls_test.reshape((10, 480, 640, -1))
+
+print('Labels loaded!')
+
+imgs_train2 = np.zeros((480, 640, 3))
+(deep_unet, name) = deep_unet(imgs_train2.shape, num_classes=5, droprate=0.0, linear=False)
+
+deep_unet.compile(optimizer='adam', loss=weighted_categorical_crossentropy(weights), metrics=['accuracy'])
+
+show_predictions(-1)
+
+history = deep_unet.fit(imgs_train, lbls_train_onehot, validation_data=[imgs_val, lbls_val_onehot],
+                  batch_size=batch_size,
+                  epochs=num_epochs,
+                  verbose=1,
+                  shuffle=True,
+                  callbacks=[DisplayCallback()])
