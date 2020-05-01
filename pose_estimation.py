6c111eadca1188552d027436230d5cd078d969e8
Jonathan Steen <joste15@student.sdu.dk>
2020-05-01 11:36:51 +0200

    Update pose estimation

--- pose_estimation.py
+++ pose_estimation.py
@@ -252,7 +252,7 @@ model.fit([lbls_train_left, lbls_train_right], poses_train,
           verbose=1,
           validation_data=([lbls_val_left, lbls_val_right], poses_val))
 
-predicted_pose = model.predict([imgs_test_left, imgs_test_right])
+predicted_pose = model.predict([lbls_test_left, lbls_test_right])
 print(poses_test[0, :, :].T)
 print(predicted_pose)
 print("DONE!")
