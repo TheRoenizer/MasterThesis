8940319746b4b2e1dc165681621d051f76264a5a
TheRoenizer <christoffer.roen@gmail.com>
2020-05-21 13:02:25 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -127,7 +127,7 @@ def load_data_EndoVis17(data_path, dtype=np.float32):
         print(str(i+1) + ' of 225')
         image_path = os.path.join(data_path, 'instrument_1_4_training/instrument_dataset_3/left_frames/frame{}.png'.format(str(i).zfill(3)))
         images_temp[i] = cv.imread(image_path).astype(dtype)
-        images[i] = images_temp[i][28:1053, 320:1601] #crop the black parts
+        images[i] = images_temp[i][28:1052, 320:1600] #crop the black parts
         images[i] = cv.normalize(images[i], dst=None, alpha=0.0, beta=1.0, norm_type=cv.NORM_MINMAX)
 
         label_path_left = os.path.join(data_path, 'instrument_1_4_training/instrument_dataset_3/ground_truth/Left_Large_Needle_Driver_labels/frame{}.png'.format(str(i).zfill(3)))
