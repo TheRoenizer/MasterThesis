c23d8042779570f31a6a889031ffc4e1eeca5fa8
Jonathan Steen <joste15@student.sdu.dk>
2020-05-21 09:41:50 +0200

    Update DeepUnet_test.py

--- DeepUnet_test.py
+++ DeepUnet_test.py
@@ -280,7 +280,7 @@ if train:
 
     deep_unet.compile(optimizer='adam', loss=weighted_categorical_crossentropy(weights), metrics=['accuracy', iou_coef, dice_coef])
 
-    tf.keras.utils.plot_model(deep_unet, to_file='model_image.png', show_shapes=True, rankdir='LR')
+    tf.keras.utils.plot_model(deep_unet, to_file='model_image.png', show_shapes=True, rankdir='RL')
 
     show_predictions(-1)
 
