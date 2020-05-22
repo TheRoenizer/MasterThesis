61e636a7efc0d2c6db161cbc118d59821c324c38
TheRoenizer <christoffer.roen@gmail.com>
2020-05-22 16:42:45 +0200

    changed from add to concat

--- Unet.py
+++ Unet.py
@@ -1,37 +1,11 @@
-
-
 try:
-    from keras.layers import Input, Conv2D, BatchNormalization, MaxPooling2D, add, UpSampling2D, Dropout, Reshape
+    from keras.layers import Input, Conv2D, BatchNormalization, MaxPooling2D, add, UpSampling2D, Dropout, Reshape, concatenate
     from keras.models import Model, load_model
 except:
     from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, MaxPooling2D, add, UpSampling2D, Dropout, Reshape
     from tensorflow.keras.models import Model
 
-'''
-def load_data(data_path, dtype=np.float32):
-    n = 99            # Number of images
-    m = 5             # Number of labels
-    dim = (480, 640)  # Image dimensions
-
-    images = np.empty((n, *dim, 3), dtype=dtype)
-    labels = np.empty((n, *dim, m), dtype=dtype)
-
-    for i in range(n):
-        image_path = os.path.join(data_path, 'Jigsaw annotations/Images/Suturing ({}).png'.format(i + 1))
-        images[i] = cv.imread(image_path).astype(dtype)
-        images[i] = cv.normalize(images[i], dst=None, alpha=0.0, beta=1.0, norm_type=cv.NORM_MINMAX)
-
-        for j in range(m):
-            label_path = os.path.join(data_path, 'Jigsaw annotations/Annotated/Suturing ({})/data/00{}.png'.format(i + 1, j))
-            labels[i, ..., j] = cv.imread(label_path, cv.IMREAD_GRAYSCALE).astype(dtype)
-            labels[i, ..., j] = cv.threshold(labels[i, ..., j], dst=None, thresh=1, maxval=255, type=cv.THRESH_BINARY)[1]
-            labels[i, ..., j] = cv.normalize(labels[i, ..., j], dst=None, alpha=0.0, beta=1.0, norm_type=cv.NORM_MINMAX)
-
-    return images, labels
-'''
-
 """Unet model for segmentation of color/greyscale images https://github.com/zhixuhao/unet"""
-
 
 def unet(input_shape, num_classes=5, droprate=None, linear=False):
     model_name = 'unet'
@@ -78,7 +52,7 @@ def unet(input_shape, num_classes=5, droprate=None, linear=False):
     # 1. up
     up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling2D(size=(2, 2))(conv5))
-    add6 = add([conv4, up6])
+    add6 = concatenate([conv4, up6], axis=-1)
     conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(add6)
     conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
     conv6 = BatchNormalization()(conv6)
@@ -86,7 +60,7 @@ def unet(input_shape, num_classes=5, droprate=None, linear=False):
     # 2. up
     up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling2D(size=(2, 2))(conv6))
-    add7 = add([conv3, up7])
+    add7 = concatenate([conv3, up7], axis=-1)
     conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(add7)
     conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
     conv7 = BatchNormalization()(conv7)
@@ -94,7 +68,7 @@ def unet(input_shape, num_classes=5, droprate=None, linear=False):
     # 3. up
     up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling2D(size=(2, 2))(conv7))
-    add8 = add([conv2, up8])
+    add8 = concatenate([conv2, up8], axis=-1)
     conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(add8)
     conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
     conv8 = BatchNormalization()(conv8)
@@ -102,7 +76,7 @@ def unet(input_shape, num_classes=5, droprate=None, linear=False):
     # 4. up
     up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling2D(size=(2, 2))(conv8))
-    add9 = add([conv1, up9])
+    add9 = concatenate([conv1, up9], axis=-1)
     conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(add9)
     conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
     conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
