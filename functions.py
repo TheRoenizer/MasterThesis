63b9daec24e886a5c6c121e9c54ee961ae2e9a5b
TheRoenizer <christoffer.roen@gmail.com>
2020-05-27 12:37:13 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -265,15 +265,15 @@ def dice(y_true, y_pred):
 
 # https://towardsdatascience.com/metrics-to-evaluate-your-semantic-segmentation-model-6bcb99639aa2
 def iou_coef_mean(y_true, y_pred, smooth=0.00001):
-    intersection = K.sum(K.abs(y_true * y_pred), axis=[0, 1, 2])
-    union = K.sum(y_true, [0, 1, 2])+K.sum(y_pred, [0, 1, 2]) - intersection
+    intersection = K.sum(K.abs(y_true * y_pred), axis=[1, 2, 3])
+    union = K.sum(y_true, [1, 2, 3])+K.sum(y_pred, [1, 2, 3]) - intersection
     iou_mean = K.mean((intersection + smooth) / (union + smooth), axis=0)
     #print("kigher: " + str(iou_mean.shape))
     return iou_mean
 
 def iou_coef0(y_true, y_pred, smooth=0.00001):
-    intersection = K.sum(K.abs(y_true * y_pred), axis=[0, 1, 2])
-    union = K.sum(y_true, [0, 1, 2])+K.sum(y_pred, [0, 1, 2])-intersection
+    intersection = K.sum(K.abs(y_true * y_pred), axis=[1, 2, 3])
+    union = K.sum(y_true, [1, 2, 3])+K.sum(y_pred, [1, 2, 3])-intersection
     iou = (intersection + smooth) / (union + smooth)
     return iou[0]
 
