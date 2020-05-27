e9838014a62b6be455495865148b06e37b479214
TheRoenizer <christoffer.roen@gmail.com>
2020-05-27 14:32:34 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -264,11 +264,10 @@ def dice(y_true, y_pred):
     return (2 * (y_true * y_pred).sum() + 1e-15) / (y_true.sum() + y_pred.sum() + 1e-15)
 
 # https://towardsdatascience.com/metrics-to-evaluate-your-semantic-segmentation-model-6bcb99639aa2
-def iou_coef_mean(y_true, y_pred, smooth=0.00001):
-    intersection = K.sum(K.abs(y_true * y_pred), axis=[1, 2, 3])
-    union = K.sum(y_true, [1, 2, 3])+K.sum(y_pred, [1, 2, 3]) - intersection
-    iou_mean = K.mean((intersection + smooth) / (union + smooth), axis=0)
-    #print("kigher: " + str(iou_mean.shape))
+def iou_coef_mean(y_true, y_pred):
+    intersection = K.sum(y_true * y_pred, axis=[0, 1, 2])
+    union = K.sum(y_true, axis=[0, 1, 2]) + K.sum(y_pred, axis=[0, 1, 2]) - intersection
+    iou_mean = K.mean((intersection + 1e-15) / (union + 1e-15), axis = 0)
     return iou_mean
 
 def iou_coef0(y_true, y_pred):
